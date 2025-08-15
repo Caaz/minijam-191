4f24b3fb27c8d106e40feb84bc81e5d1e4a03e43
Daniel Cavazos <daniel@caaz.me>
2025-08-15 15:06:40 -0700

    hook up score, set gam scene as entrypoint

--- darknightskystuff/prototype.gd
+++ darknightskystuff/prototype.gd
@@ -1,10 +1,16 @@
-extends Node3D
+class_name Prototype extends Node3D
+
+signal score_changed(score:int)
 
 @export var crate_types: Dictionary[String, Mesh]
 @export var foods: Dictionary[String, Mesh]
 @export var crate_positions: Array[Vector3]
 var start_num_crates: int = 1
 
+var score:int = 0:
+	set(new_score):
+		score = new_score
+		score_changed.emit(score)
 
 var current_crate: CharacterBody3D
 
@@ -13,6 +19,9 @@ func _ready() -> void:
 	var crate = preload("res://darknightskystuff/custom_crate.tscn").instantiate()
 	crate.position = crate_positions.pick_random()
 	crate.get_child(0).mesh = crate_types.values().pick_random()
+	crate.food_captured.connect(func(points:int):
+		score += points
+	)
 	add_child(crate)
 
 func _input(event: InputEvent) -> void:
--- scenes/gameplay_ui/gameplay_ui.gd
+++ scenes/gameplay_ui/gameplay_ui.gd
@@ -2,7 +2,11 @@ class_name GameplayUI extends Control
 
 @export var score_label:Label
 @export var time_label:Label
+@export var prototype:Prototype
 
-func ready() -> void:
-	# This is where we'd connect to signals to update the score and time labels.
-	pass
+func _ready() -> void:
+	print(prototype)
+	prototype.score_changed.connect(func(new_score:int):
+		print("Score changed and caught")
+		score_label.text = "Score: %05d" % new_score
+	)
