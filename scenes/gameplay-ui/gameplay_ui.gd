6532743fbb8f0e1f8afbf223486b0ee914ad50ce
Daniel Cavazos <daniel@caaz.me>
2025-08-15 13:42:29 -0700

    Move comment.

--- scenes/gameplay-ui/gameplay_ui.gd
+++ scenes/gameplay-ui/gameplay_ui.gd
@@ -4,5 +4,5 @@ class_name GameplayUI extends Control
 @export var time_label:Label
 
 func ready() -> void:
-	pass
 	# This is where we'd connect to signals to update the score and time labels.
+	pass
