e3fe205ac67e094714be62fe41f822547d172848
HasanBaranK <hasanbaran98@gmail.com>
2025-08-15 22:09:20 +0100

    Food is now collectable

---
+++ darknightskystuff/falling_food.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+func _ready() -> void:
+	#var material = $FloorIndicator.get_active_material(0)
+	#$FloorIndicator.material_override = material
+	#$FloorIndicator.material_override.albedo_color.a = 0.1
+	#print($FloorIndicator.material_override.albedo_color)
+	add_to_group("food")
+	pass
+
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	$Falling/Area3D.queue_free()
+	$FloorIndicator.queue_free()
+	pass # Replace with function body.
