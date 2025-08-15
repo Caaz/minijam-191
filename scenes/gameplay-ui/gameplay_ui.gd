0dacf6aace9bbebaa9e4d5b515373a6ad650ab48
Daniel Cavazos <daniel@caaz.me>
2025-08-15 13:41:21 -0700

    Add font, basic ui, and gameplay scene.

---
+++ scenes/gameplay-ui/gameplay_ui.gd
@@ -0,0 +1,8 @@
+class_name GameplayUI extends Control
+
+@export var score_label:Label
+@export var time_label:Label
+
+func ready() -> void:
+	pass
+	# This is where we'd connect to signals to update the score and time labels.
